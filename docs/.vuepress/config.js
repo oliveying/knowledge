9d28b00de819339882a827a06835367303b87cd2
xysong <songxiaoying0@163.com>
2019-03-25 16:15:43 +0800

    update docs html

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,39 +3,50 @@ module.exports = {
   title: '杜若的前端，每天进步一点点',  // 设置网站标题
   dest: './dist',   // 设置输出目录
   base: '/knowledge/', // 设置站点根路径
-  repo: 'https://github.com/oliveying/knowledge' // 添加 github 链接
-  // locales: {
-  //   '/': {
-  //     lang: 'zh-CN',
-  //     title: 'duruo',
-  //     description: '每天进步一点点'
-  //   }
-  // },
-  // themeConfig: {
-  //   // 添加导航栏
-  //   nav: [
-  //     { text: 'vue', link: '/' },
-  //     { text: 'css', link: '/blog/' },
-  //     { text: 'js', link: '/zhihu/' },
-  //     {
-  //       text: 'github'
-  //       // 这里是下拉列表展现形式。
-  //       // items: [
-  //       //   { text: 'focus-outside', link: 'https://github.com/TaoXuSheng/focus-outside' },
-  //       //   { text: 'stylus-converter', link: 'https://github.com/TaoXuSheng/stylus-converter' }
-  //       // ]
-  //     }
-  //   ],
-  //   为以下路由添加侧边栏
-  //   sidebar: {
-  //     '/vue/': [
-  //       'one',
-  //       'two'
-  //     ],
-  //     '/css/': [
-  //       'three',
-  //       'four'
-  //     ]
-  //   }
-  // }
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'duruo',
+      description: '每天进步一点点'
+    }
+  },
+  themeConfig: {
+    repo: 'https://github.com/oliveying/knowledge', // 添加 github 链接
+    // head: ['link', {
+    //   rel: 'icon',
+    //   href: '/favicon.ico'
+    // }],
+    // 添加导航栏
+    nav: [
+      {text: 'Home', link: '/'},
+      {
+        text: '前端技术',
+        // 这里是下拉列表展现形式。
+        items: [
+          {text: 'html', link: '/html/'},
+          {text: 'css兼容性', link: '/css_compatibility/'},
+          // { text: 'js', link: '/js/' },
+          {text: 'h5最新用法', link: '/h5/'},
+        ]
+      },
+      {
+        text: '扩展', items: [
+          {text: 'git用法', link: '/git/'},
+          {text: 'markDown', link: '/markdown/'},
+        ]
+      },
+      {text: '测试', link: '/test_tool/'},
+    ]
+  },
+    // 为以下路由添加侧边栏
+    // sidebar: {
+    //   '/vue/': [
+    //     'one',
+    //     'two'
+    //   ],
+    //   '/css/': [
+    //     'three',
+    //     'four'
+    //   ]
+    // }
 };
--- docs/git/README.md
+++ docs/git/README.md
@@ -1,6 +1,6 @@
 ## 管理git生成的多个ssh key
 
-[来自简书](https://www.jianshu.com/p/f7f4142a1556)
+> [来自简书](https://www.jianshu.com/p/f7f4142a1556)
 ### 问题阐述
 
 * 当有多个git账号的时候，比如一个github，用于自己进行一些开发活动，再来一个gitlab，一般是公司内部的git。这两者你的邮箱如果不同的话，就会涉及到一个问题，生成第二个git的key的时候会覆盖第一个的key，导致必然有一个用不了。
@@ -192,19 +192,19 @@
 
 ## git reset 的具体用法
 
-<font color="red">git reset [--hard|soft|mixed|merge|keep] [<commit>或HEAD]</font>
+<font color="red">git reset [--hard|soft|mixed|merge|keep] [\<commit\>或HEAD]</font>
 
-* 作用：将当前分支reset到指定的<commit>或者HEAD(默认为最新的一次提交，即重设到最新一次提交之前的版本)
+* 作用：将当前分支reset到指定的\<commit\>或者HEAD(默认为最新的一次提交，即重设到最新一次提交之前的版本)
 
 index，执行git add的操作，会对文件创建索引，所有被跟踪的文件索引会放入index，表示文件被修改待提交
 working tree，当前工作区，被修改但未被add的文件，存储在工作区
 ORIG_HEAD,用于指向前一个操作状态,每次的commit或者pull或者reset，git 都会把老的HEAD拷贝到.git/ORIG_HEAD，通过对ORIG_HEAD的引用可以指向前一次的操作状态
 
 1. hard(慎用)
-* 重设index和working tree,所有改变都会被丢弃，包括文件的修改、新增、删除等操作，并把HEAD指向<commit>，
+* 重设index和working tree,所有改变都会被丢弃，包括文件的修改、新增、删除等操作，并把HEAD指向\<commit>，
 因此通过git log查看版本提交记录，被reset的版本记录会被丢弃，但可以通过git reflog查看
 2. soft
-* 不重设index和working tree,仅仅将HEAD指向<commit>,表示已经commit的文件会取消commit,
+* 不重设index和working tree,仅仅将HEAD指向\<commit>,表示已经commit的文件会取消commit,
 通过git status查看，文件会处于待commit状态“Changes to be committed”
 3. mixed(默认)
 * 重设index,但不重设working tree,表示已经被add的文件，被取消add，

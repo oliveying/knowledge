8ab40bf6a8fc3285a255991e0b6576f6fc64de86
xysong <songxiaoying0@163.com>
2019-03-28 18:29:18 +0800

    add tongji_ga

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">开心哟</div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      return {}
+    },
+    mounted() {
+      this.tongji_ga();
+    },
+    methods: {
+      // ga统计
+      tongji_ga() {
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-137025177-1');
+
+        var userAgent = navigator.userAgent;
+        if (userAgent.indexOf('ignore') < 0) {
+          var ga = document.createElement("script");
+          ga.async = true;
+          ga.src = "//www.googletagmanager.com/gtag/js?id=UA-137025177-1";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(ga, s);
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- docs/css_compatibility/README.md
+++ docs/css_compatibility/README.md
@@ -1,4 +1,12 @@
-#### 解决input表单元素在ios系统上默认圆角的问题
+### 解决input表单元素在ios系统上默认圆角的问题
+```
+ -webkit-appearance: none ;
+ border-radius: 2px;
+```
 
-         -webkit-appearance: none ;
-         border-radius: 2px;
+### table的td里如果是比较长的数字，会影响样式，不会换行，所以需要加强制换行
+```
+white-space:normal;
+word-break:break-all;
+```
+
